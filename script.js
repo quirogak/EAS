64ccd6f3296cd2e83ba428d4f2c1c586f305d887
quirogak <quirogakill@gmail.com>
2022-08-17 16:30:49 -0500

    Changed from Flexbox to CSS Grid.
    After trying to make the grid with Flexbox, i realized that CSS Grid is much better for making grids and setting them up using JS.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const container = document.querySelector("#container")
+const gridSize = prompt("please select a number between 1 and 100")
 
-for (let i = 0;i < 361 ; i++){
+
+
+for (let i = 0;i < (16*16) ; i++){
 
     const div = document.createElement("div");
     container.appendChild(div);
     div.setAttribute('id','divs')
-
+    container.style.gridTemplateColumns = "repeat("+ gridSize +","+ (1900/gridSize) +"px)";
+    container.style.gridTemplateRows = "repeat("+ gridSize +","+ (900/gridSize) +"px)";
+    
+   
 
 
     
