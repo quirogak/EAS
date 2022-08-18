11c610f992bc2e389b5b81b06021dc67273ccb8c
quirogak <quirogakill@gmail.com>
2022-08-18 12:51:04 -0500

    Finalized the project. Found a way to restart the divs, using a eventListener inside the function that restarts the old divs and replace them.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EAS
 Etch-a-Sketch
+
+After finalizing the project i realized that it was really important in order to understand the DOM, adding nodes to a parent node, altering them with style changes and using eventListeners.
+This is the first project that synergizes HTML, CSS and JS, so i learned a lot in the process.
+
+I've always used flexbox, but in this project that requires to make an actual Grid, i had to learn some basics of CSS Grid, which is really helpful, because i realized that i can use Flexbox in some things and use CSS Grid that is better in another things.
--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 const container = document.querySelector("#container")
-const gridSize = prompt("Please select a number between 1 and 100")
+const header = document.querySelector("#header")
 
-
+//-----------------------------------------------------
 
 const createDivs = function(){
+
+const gridSize = prompt("Please select the size of the canvas (1 - 100)")
+
+
 
 if (gridSize > 100 || gridSize < 1 ) {
   alert("Please put a valid number") 
   window.location.reload()
+  
 }
-
 else {
-
 for (let i = 0;i < (gridSize*gridSize) ; i++){
 
     const div = document.createElement("div");
     container.appendChild(div);
     div.setAttribute('id','divs')
     container.style.gridTemplateColumns = "repeat("+ gridSize +","+ (1100/gridSize) +"px)";
-    container.style.gridTemplateRows = "repeat("+ gridSize +","+ (900/gridSize) +"px)";
-    
-    div.addEventListener('mouseover', () => {
+    container.style.gridTemplateRows = "repeat("+ gridSize +","+ (850/gridSize) +"px)";
+    div.addEventListener('mouseenter', () => {
+    div.style.backgroundColor = "black"
 
-     div.style.backgroundColor = "black"
     });
-   
-
-
-}  
+    //each time the button is clicked, the current divs are replaced to new divs, in order to have a new canvas each time we press the button.
+  refreshButton.addEventListener('click',()=>{
+    div.remove()
+  });  
 }
 }
+}
 
-console.log(createDivs())
+//-------------------------------------------------------------
+const refreshButton = document.createElement("button")
+header.appendChild(refreshButton)
+refreshButton.setAttribute('id','refreshButton')
+refreshButton.textContent="Start a new canvas"
+refreshButton.addEventListener('click',createDivs);
 //--------------------------------------------------------
